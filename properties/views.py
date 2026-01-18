3fe89cfdd1e6aa28234aa1952489a3768914c86d
Hilda Enyioko <enyiokohilda@gmail.com>
2026-01-18 16:41:50 +0100

    update property_list view to retrieve data from the cache

--- properties/views.py
+++ properties/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
+from .utils import get_all_properties
 from .models import Property
 
 @cache_page(60 * 15)  # Cache for 15 minutes
 def property_list(request):
-    properties = Property.objects.all().values(
+    properties = get_all_properties().values(
         'id',
         'title',
         'description',
